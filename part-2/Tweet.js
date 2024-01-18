b3e35937a08b58e1c05d95357831dfd691c6f6ea
aovalles2711 <124206685+aovalles2711@users.noreply.github.com>
2024-01-18 17:23:02 -0600

    Add files via upload

---
+++ part-2/Tweet.js
@@ -0,0 +1,12 @@
+/** Props: name, username, data, message */
+
+function Tweet({ name, username, date, message }) {
+    return (
+        <div className="tweet">
+            <span>{name}</span>
+            <span className="username">{username}</span>
+            <span className="date">{date}</span>
+            <p>{message}</p>
+        </div>
+    );
+}
